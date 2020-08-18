5497d8052431fbbcb9027f0a916f943b3d182dfb
John Foster <john44foster@aol.com>
2020-08-18 15:08:28 +0100

    Updated Decimal Button Functionality

--- Calculator/app.js
+++ Calculator/app.js
@@ -10,69 +10,71 @@ function clicked(e) {
     const action = key.dataset.action;
     const keyContent = key.textContent;
     const displayNum = display.textContent;
+    const previousKeyType = calculator.dataset.previousKeyType;
 
+// NUMBERS ________________________________________________________________
     if (!action) {
-      const previousKeyType = calculator.dataset.previousKeyType
-      Array.from(key.parentNode.children).forEach(k => k.classList.remove('depressed'));
+      calculator.dataset.previousKeyType = "number";
 
-      if (displayNum === "0" || previousKeyType === 'operator') {
+      Array.from(key.parentNode.children).forEach((k) =>
+        k.classList.remove("depressed")
+      );
+
+      if (displayNum === "0" || previousKeyType === "operator") {
         display.textContent = keyContent;
       } else {
         display.textContent = displayNum + keyContent;
       }
     }
-
+// OPERATORS ( +, /, *, -) ________________________________________________
     if (
       action === "add" ||
       action === "divide" ||
       action === "multiply" ||
       action === "subtract"
     ) {
-      key.classList.add('depressed');
-      calculator.dataset.previousKeyType = 'operator';
-
+      key.classList.add("depressed");
+      calculator.dataset.previousKeyType = "operator";
       calculator.dataset.firstValue = displayNum;
       calculator.dataset.operator = action;
     }
-
+// EQUALS _________________________________________________________________
     if (action === "equals") {
       const firstValue = calculator.dataset.firstValue;
       const operator = calculator.dataset.operator;
       const secondValue = displayNum;
+      calculator.dataset.previousKeyType = "equals";
 
       const calculate = (n1, operator, n2) => {
-        let result = '';
+        let result = "";
 
-        if (operator === 'add'){
+        if (operator === "add") {
           result = parseFloat(n1) + parseFloat(n2);
-        } else if (operator === 'subtract'){
+        } else if (operator === "subtract") {
           result = parseFloat(n1) - parseFloat(n2);
-        } else if (operator === 'divide'){
+        } else if (operator === "divide") {
           result = parseFloat(n1) / parseFloat(n2);
-        } else if (operator === 'multiply'){
+        } else if (operator === "multiply") {
           result = parseFloat(n1) * parseFloat(n2);
         }
-
         return result;
-      }
-
-      display.textContent  = calculate(firstValue, operator, secondValue)
-      console.log("equals");
+      };
+      display.textContent = calculate(firstValue, operator, secondValue);
     }
-
+// DECIMAL ________________________________________________________________
     if (action === "decimal") {
-      console.log("decimal");
-      display.textContent = displayNum + '.';
-      if (!displayNum.includes('.')){
-        display.textContent = displayNum + '.';
+      calculator.dataset.previousKeyType = "decimal";
+
+      if (!displayNum.includes(".")) {
+        display.textContent = displayNum + ".";
+      } else if (previousKeyType === "operator") {
+        display.textContent = "0.";
       }
     }
-
+// AC (CLEAR ALL/ALL CLEAR) _______________________________________________
     if (action === "clear") {
-      console.log("clear");
-      display.textContent = '0';
+      calculator.dataset.previousKeyType = "clear";
+      display.textContent = "0";
     }
-
-    
   }
 }
